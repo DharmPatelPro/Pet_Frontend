26b2185991521c5566f175f3db3b217cde9a080d
Dharm Patel <108889488+DharmPatelPro@users.noreply.github.com>
2023-06-12 13:35:00 +0530

    aaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
             </svg>
           </Link>}
           {/* {localStorage.getItem('token') && <p>log out</p>} */}
-          {localStorage.getItem('token') &&<Link to="/login" onClick={() => { localStorage.removeItem("token").then(window.location.reload()) }} class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login Out
+          {localStorage.getItem('token') &&<Link to="/login" onClick={() => { localStorage.removeItem("token").then(window.location.reload()) }} class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Log Out
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
